7edefe3c15bf16297836876603e4e1791b64e616
Cristina Coves Mora <kylie_212@hotmail.com>
2018-10-01 16:07:03 +0200

    [+TASK] added component 'Logo.js'

--- practicas/sesion-05/src/App.js
+++ practicas/sesion-05/src/App.js
@@ -1,7 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+import Logo from './Logo.js';
+
+export default class App extends Component {
+
+    constructor ( ...args ) {
+        super( ...args )
+        this.state = {}
+    }
+
+    render () {
+        return (
+            <div className='container' >
+               <Logo isCentered />
+            </div>
+        );
+    }
 }
 
-export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ practicas/sesion-05/src/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Logo ( props ) {
+    const className = props.isCentered ? 'has-text-centered' : '';
+    return (
+        <div className={className}>
+            <img src="marvel-logo.jpeg" alt="marvel logo" role='presentation'/>
+        </div>
+    )
+}
+
+Logo.propTypes = {
+    isCentered: PropTypes.bool
+}
